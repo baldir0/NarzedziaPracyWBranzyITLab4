73dfaf10567f929bda5112550c0742bfcbf3977c
baldir0 <lewucha2000@gmail.com>
2022-07-10 09:41:44 +0200

    zad1.3

--- skrypt.sh
+++ skrypt.sh
@@ -4,7 +4,13 @@ if [ $1 = "--date" ] ; then
 	echo $(date)
 elif [ $1 = "--logs" ] ; then
 	iterator=0
-	while [ $iterator -lt 100 ] ; do
+	max=100
+	
+	if [ ! -z $2 ] ; then
+		max=$2
+	fi
+	
+	while [ $iterator -lt $max ] ; do
 		echo log$iterator.txt $(date) > log$iterator.txt
 		iterator=$[iterator + 1]
 	done
