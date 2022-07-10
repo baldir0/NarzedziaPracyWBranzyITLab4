31b23842eda3f7ba3b80f02f15c580dfb4a00cb8
baldir0 <lewucha2000@gmail.com>
2022-07-10 10:07:51 +0200

    zad2.1

--- skrypt.sh
+++ skrypt.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ $1 = "--date" ] ; then
+if [ $1 = "--date" ] || [ $1 = "-d" ] ; then
 	echo $(date)
-elif [ $1 = "--logs" ] ; then
+elif [ $1 = "--logs" ] || [ $1 = "-l" ] ; then
 	iterator=0
 	max=100
 	
-	if [ ! -z $2 ] ; then
+	if [ ! -z $2] ; then
 		max=$2
 	fi
 	
@@ -14,9 +14,12 @@ elif [ $1 = "--logs" ] ; then
 		echo log$iterator.txt $(date) > log$iterator.txt
 		iterator=$[iterator + 1]
 	done
-elif [ $1 = "--help" ] ; then
-	echo "--date - wyświetl aktualną date"
-	echo "--log - utwórz 100 plików log"
-	echo "--log <number> - utwórz x plików log"
-	echo "--help - wyświetl tą wiadomość"
+elif [ $1 = "--help" ] || [ $1 = "-h" ] ; then
+	echo "[--date || -d] - wyświetl aktualną date"
+	echo "[--log || -l] - utwórz 100 plików log"
+	echo "[--log || -l] <number> - utwórz x plików log"
+	echo "[--help || -h] - wyświetl tą wiadomość"
+elif [ $1 = "--init" ] ; then
+	$(git clone https://github.com/baldir0/NarzedziaPracyWBranzyITLab4.git)
+	PATH=$PATH:/($pwd)
 fi
